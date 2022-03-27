9c7add6b048223241d622e1313f0ba6a3dedf6f8
Sarry-Mac <sarryqassir@hotmail.com>
2022-03-27 11:37:58 -0400

    setup navbar

---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import React from "react";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        {/* Link component from next uses client side routing instead of a traditional anchor */}
+        <NextLink href="/login">
+          <Link mr={2}>login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>register</Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
