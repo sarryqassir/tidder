ebad73ec018aee7768d4e7d75be5c481c04baeeb
Sarry-Mac <sarryqassir@hotmail.com>
2022-03-27 12:01:32 -0400

    implemented logout with cache updating

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -10,6 +10,7 @@ import {
   Query,
   Resolver,
 } from "type-graphql";
+import { COOKIE_NAME } from "../constants";
 import { User } from "../entities/User";
 import { MyContext } from "../types";
 
@@ -152,5 +153,21 @@ export class UserResolver {
     req.session.userId = user.id;
 
     return { user };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
   }
 }
