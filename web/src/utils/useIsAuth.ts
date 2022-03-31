abd03382a04050be999692dc762b89ef51650226
Sarry-Mac <sarryqassir@hotmail.com>
2022-03-30 20:38:11 -0400

    nextjs query parameter

--- web/src/utils/useIsAuth.ts
+++ web/src/utils/useIsAuth.ts
@@ -5,10 +5,9 @@ import { useMeQuery } from "../generated/graphql";
 export const useIsAuth = () => {
   const [{ data, fetching }] = useMeQuery();
   const router = useRouter();
-  console.log(router);
   useEffect(() => {
     if (!fetching && !data?.me) {
-      router.replace("/login");
+      router.replace("/login?next=" + router.pathname);
     }
   }, [fetching, data, router]);
 };
