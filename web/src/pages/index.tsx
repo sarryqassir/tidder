d456a25f0e0b5fee28495c2217f04f39f1006d74
Sarry-Mac <sarryqassir@hotmail.com>
2022-03-30 22:18:51 -0400

    added some styling with chakra

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,12 @@
-import { Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 import React from "react";
@@ -6,25 +14,46 @@ import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
+
+  if (!fetching && !data) {
+    return <div>query failed</div>;
+  }
+
   return (
-    <React.StrictMode>
-      <Layout>
+    <Layout>
+      <Flex align="center">
+        <Heading>Tidder</Heading>
         <NextLink href="/create-post">
-          <Link>create post</Link>
+          <Link ml="auto">create post</Link>
         </NextLink>
-        <br />
-        {!data ? (
-          <div>loading...</div>
-        ) : (
-          data.posts.map((p) => <div key={p.id}>{p.title}</div>)
-        )}
-      </Layout>
-    </React.StrictMode>
+      </Flex>
+      <br />
+      {!data && fetching ? (
+        <div>loading...</div>
+      ) : (
+        <Stack spacing={8}>
+          {data!.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              {/* //TODO: nicer slice with ... */}
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data ? (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={8}>
+            load more
+          </Button>
+        </Flex>
+      ) : null}
+    </Layout>
   );
 };
 
